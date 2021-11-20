061d5c653ceac19bc9cd83ec7bec55f31dade5bf
Andrzej Szombierski <qq@kuku.eu.org>
2021-11-20 15:54:40 +0100

    emu: persist emulated eeprom to file

--- firmware/SW102/src/emu/eeprom_hw.c
+++ firmware/SW102/src/emu/eeprom_hw.c
@@ -10,15 +10,28 @@
 #include "eeprom_hw.h"
 #include "common.h"
 #include "assert.h"
+#include <stdio.h>
+
+FILE *eeprom;
 
 // returns true if our preferences were found
 bool flash_read_words(void *dest, uint16_t length_words)
 {
-	return false;
+	if(!eeprom)
+		return false;
+
+	fseek(eeprom, 0, 0);
+	fread(dest, length_words, 4, eeprom);
+	return true;
 }
 
 bool flash_write_words(const void *value, uint16_t length_words)
 {
+	if(!eeprom)
+		return false;
+	fseek(eeprom, 0, 0);
+	fwrite(value, length_words, 4, eeprom);
+	fflush(eeprom);
 	return true;
 }
 
@@ -28,5 +41,8 @@ bool flash_write_words(const void *value, uint16_t length_words)
  */
 void eeprom_hw_init(void)
 {
+	eeprom = fopen("eeprom.bin", "r+b");
+	if(!eeprom)
+		eeprom = fopen("eeprom.bin", "w+b");
 }
 
